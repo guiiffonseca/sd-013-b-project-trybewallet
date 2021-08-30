1405ab3e27f74635d3f8e883ee32a8fffd4d8b94
Aleilton Chavenco <aleilton@gmail.com>
2021-08-30 14:33:59 -0300

    Iniciando o projeto

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,5 @@
 // import wallet from './wallet';
 
 // Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e
+// "wallet" no seu estado global
