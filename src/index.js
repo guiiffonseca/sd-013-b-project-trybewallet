4e49d0cf4572d5fc39781ea9200a179a55273e08
Alex <alexcorreas1994@gmail.com>
2021-08-30 19:04:24 -0300

    Colocando o Provider

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    {/* <Provider store={ store }> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
