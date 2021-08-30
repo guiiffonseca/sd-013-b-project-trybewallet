3e718ec8a0a7c78615dd0317ec46628c830f9f48
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-08-30 16:37:43 -0300

    lint novamente

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import loginAction from '../actions';
 import PropTypes from 'prop-types';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -22,58 +22,57 @@ class Login extends React.Component {
       [name]: value,
     });
     const { email, password } = this.state;
-    const number = [-1, 5];
-    if (email.indexOf('@') !== number[0] && password.length >= number[1]) {
-      if (email.indexOf('.com') !== number[0]) {
-        this.setState({ button: false });
-      };
-    }
-    else this.setState({ button: true });
+    const numberOne = -1;
+    const numberFive = 5;
+    if (email.indexOf('@') !== numberOne && password.length >= numberFive) {
+      if (email.indexOf('.com') !== numberOne) this.setState({ button: false });
+    } else this.setState({ button: true });
   }
 
   render() {
     const { email, password, button } = this.state;
     const { loginValues, history } = this.props;
-    return (<div className="login-field">
-      <form>
-        <input
-          type="email"
-          value={ email }
-          name="email"
-          data-testid="email-input"
-          placeholder="Digite seu email..."
-          onChange={ this.loginChanges }
-        />
-        <input
-          type="password"
-          value={ password }
-          name="password"
-          data-testid="password-input"
-          placeholder="Digite sua senha..."
-          onChange={ this.loginChanges }
-        />
-        <button
-          type="button"
-          disabled={ button }
-          onClick={ () => {
-            loginValues(email);
-            history.push('/carteira')
-          } }
-        >
-          Entrar
-        </button>
-      </form>
-    </div>)
+    return (
+      <div className="login-field">
+        <form>
+          <input
+            type="email"
+            value={ email }
+            name="email"
+            data-testid="email-input"
+            placeholder="Digite seu email..."
+            onChange={ this.loginChanges }
+          />
+          <input
+            type="password"
+            value={ password }
+            name="password"
+            data-testid="password-input"
+            placeholder="Digite sua senha..."
+            onChange={ this.loginChanges }
+          />
+          <button
+            type="button"
+            disabled={ button }
+            onClick={ () => {
+              loginValues(email);
+              history.push('/carteira');
+            } }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>);
   }
-};
+}
 
 const mapDispatchToProps = (dispatch) => ({
   loginValues: (email) => dispatch(loginAction(email)),
-})
+});
 
 Login.propTypes = {
   loginValues: PropTypes.func.isRequired,
   history: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+};
 
 export default connect(null, mapDispatchToProps)(Login);
