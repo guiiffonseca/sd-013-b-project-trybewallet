07512d8afe9772267889ca4715055a5d544568ba
Jessica Naiara <jessicanaiarag@gmail.com>
2021-08-30 19:13:12 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <fieldset>
+          <input
+            data-testid="email-input"
+            type="text"
+            onChange=""
+            value=""
+            name="email"
+            placeholder="alguem@alguem.com"
+            required
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            onChange=""
+            value=""
+            name="password"
+            placeholder="Digite sua senha"
+            minLength="6"
+            required
+          />
+          <button type="button" onClick=""> Entrar </button>
+        </fieldset>
+      </form>
+    );
   }
 }
 
