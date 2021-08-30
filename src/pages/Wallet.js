d0d130e8dd190ce1a87639c19bd2d9c568ada982
Henrique Lima <hnamix@gmail.com>
2021-08-30 14:48:19 -0400

    atualiza os reducers

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { string } from 'prop-types';
 import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
-    console.log(email);
     return (
       <section>
         <header>
           <div>TrybeWallet</div>
-          <h3 data-testid="email-field">{ email }</h3>
-          <h3 data-testid="total-field">0</h3>
+          <h3 data-testid="email-field">Email</h3>
+          <h3 data-testid="total-field">Total Despesas</h3>
           <h3 data-testid="header-currency-field">BRL</h3>
         </header>
         <Form />
@@ -20,12 +16,4 @@ class Wallet extends React.Component {
     );
   }
 }
-
-Wallet.propTypes = {
-  email: string,
-}.isRequired;
-
-const mapStateToProps = (state) => ({
-  email: state.user.email });
-
-export default connect(mapStateToProps, null)(Wallet);
+export default Wallet;
