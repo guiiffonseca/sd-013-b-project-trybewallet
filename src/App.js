85395721224b33bf8c5f2031db230894d3562507
giiffonseca <guii.ffonseca22@gmail.com>
2021-08-30 17:29:36 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router';
+import Login from './pages/Login'
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      Hello, TrybeWallet!
+      <switch>
+        <Route exact path="/" component={ Login } />
+      </switch>
+    </div>
+  ) 
 }
 
 export default App;
