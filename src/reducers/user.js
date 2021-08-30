4f0382b899dd1727142bcf774debcf4192068976
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-30 16:36:43 -0300

    Implementando a Store e Concluindo requesito 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+  case 'USER_LOGIN':
+    return action.value;
+  default:
+    return state;
+  }
+};
+
+export default user;
