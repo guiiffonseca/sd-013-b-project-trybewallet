24385fdc942328ab9a79a96e360a6157ad15c1d9
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-30 15:48:19 -0300

    Requisito 1

---
+++ src/components/Input.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+  render() {
+    const { id, type, placeholder, test, onChange } = this.props;
+    return (
+      <input
+        id={ id }
+        type={ type }
+        placeholder={ placeholder }
+        data-testid={ test }
+        onChange={ onChange }
+      />
+    );
+  }
+}
+
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  test: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Input;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Input from '../components/Input';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput({ target }) {
+    this.setState({
+      email: target.value,
+    });
+    console.log(this.state);
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <>
+        <Input
+          id="email"
+          type="email"
+          placeholder="Digite seu email"
+          test="email-input"
+          onChange={ this.handleInput }
+        />
+        <Input
+          id="passwaord"
+          type="password"
+          placeholder="Digite sua senha"
+          test="password-input"
+        />
+        <button type="button">
+          <Link to="/carteira">
+            Entrar
+          </Link>
+        </button>
+      </>
+    );
   }
 }
 
