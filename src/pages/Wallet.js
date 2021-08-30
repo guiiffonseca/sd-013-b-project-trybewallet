385ab8f1c8a97806f0dbb869a4ca1511b61c2871
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-30 18:38:23 -0300

    Requisito 5 ~hard coded xO

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    const TOTAL = 0;
+    const CURRENCY = 'BRL';
+
+    return (
+      <header>
+        <span data-testid="email-field">
+          { `Email: ${email} `}
+        </span>
+        <span data-testid="total-field">
+          {`Despesa Total: ${TOTAL}`}
+        </span>
+        <span data-testid="header-currency-field">
+          { CURRENCY }
+        </span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
