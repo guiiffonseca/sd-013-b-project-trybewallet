bb16a44ff90633cfabaf0eb5bdb70622ada42742
Henrique Lima <hnamix@gmail.com>
2021-08-30 14:18:47 -0400

    req01 sera que passa?

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          Email
+          <input
+            data-testid="email-input"
+            placeholder="Insira seu email"
+            id="email"
+            type="email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            data-testid="password-input"
+            placeholder="Insira uma senha"
+            id="password"
+            type="text"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
-
 export default Login;
