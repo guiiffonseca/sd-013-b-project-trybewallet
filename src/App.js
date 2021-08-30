59f7c014557d2f6b71282d2f2a8c2dd0a3317393
Alessandro Hugen <alessandrohugen@gmail.br>
2021-08-30 18:53:25 -0300

    LINT

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './Components/Login.jsx';
+import Login from './Components/Login';
 
 function App() {
   return (
