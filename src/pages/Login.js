ff2a740086158e57a21943727c11b58566af9387
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-30 18:21:20 -0300

    Requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
+import { getUserEmail } from '../actions';
 import Input from '../components/Input';
 
 class Login extends React.Component {
@@ -10,9 +13,11 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      logged: false,
     };
 
     this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInput({ target }) {
@@ -22,11 +27,23 @@ class Login extends React.Component {
     });
   }
 
+  handleSubmit() {
+    const { email } = this.state;
+    const { userEmail } = this.props;
+    userEmail(email);
+    this.setState({
+      logged: true,
+    });
+  }
+
   render() {
-    const { email, password } = this.state;
+    const { email, password, logged } = this.state;
     const PASSWORD_MINIMUM_CHAR = 6;
     // regex: https://www.w3resource.com/javascript/form/email-validation.php
     const EMAIL_VALIDATION = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (logged) return <Redirect to="/carteira" />;
+
     return (
       <fieldset>
         <form>
@@ -49,11 +66,10 @@ class Login extends React.Component {
           <button
             type="button"
             disabled={ password.length < PASSWORD_MINIMUM_CHAR
-              || !EMAIL_VALIDATION.test(email) }
+            || !EMAIL_VALIDATION.test(email) }
+            onClick={ this.handleSubmit }
           >
-            <Link to="/carteira">
-              Entrar
-            </Link>
+            Entrar
           </button>
         </form>
       </fieldset>
@@ -61,4 +77,12 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  userEmail: (email) => dispatch(getUserEmail(email)),
+});
+
+Login.propTypes = {
+  userEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
