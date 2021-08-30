1401703e2a827875df9cb2123768c53248de7682
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-30 17:54:50 -0300

    Requisito 6 Finalizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <p>
-          <span data-testid="email-field">{ email }</span>
-          <span data-testid="total-field">0</span>
-          <span data-testid="header-currency-field">BRL</span>
-        </p>
-      </header>
+      <div>
+        <header>
+          <p>
+            <span data-testid="email-field">{ email }</span>
+            <span data-testid="total-field">0</span>
+            <span data-testid="header-currency-field">BRL</span>
+          </p>
+        </header>
+        <WalletForm />
+      </div>
     );
   }
 }
