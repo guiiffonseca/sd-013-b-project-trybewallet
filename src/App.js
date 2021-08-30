e62accb7d35ff68d22c31c7e105b771008015ca2
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-08-30 22:46:05 +0100

    requisito 1 finalizado e store criado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import './App.css';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/wallet" component={ Wallet } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login">
+        <section className="emailInput">
+          <input
+            type="email"
+            //  onChange={ handleChange }
+            placeholder="email"
+            data-testid="email-input"
+          />
+          <input
+            type="text"
+            //  onChange={ handleChange }
+            placeholder="password"
+            data-testid="password-input"
+          />
+        </section>
+        <div className="link">
+          <Link
+            to="/wallet"
+            //  onClick={ () => login({ user.email, user.password }) }
+          >
+            Entrar
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
