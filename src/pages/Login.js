b2cf2420b0dc8edcfe58c95f9456671c5a7a45d1
bvottri <bruna.votre@gmail.com>
2021-08-30 20:35:16 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,62 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDisabled = this.handleDisabled.bind(this);
+    this.state = {
+      email: '',
+      senha: '',
+      // button: 'true',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleDisabled() {
+    const { senha, email } = this.state;
+    const rgx = /\S+@\S+\.\S+/;
+    const lengthPassword = 5;
+    if (rgx.test(email) && senha.length > lengthPassword) {
+      return true;
+    }
+  }
+
   render() {
+    const { email, senha } = this.state;
     return (
       <div>
         <fieldset>
           Email
-          <input data-testid="email-input" name="login" type="text" label="email" />
+          <input
+            onChange={ this.handleChange }
+            data-testid="email-input"
+            value={ email }
+            name="email"
+            type="email"
+            label="email"
+          />
           Senha
-          <input data-testid="password-input" name="senha" type="password" />
-          <button type="submit" label="button">Entrar</button>
+          <input
+            onChange={ this.handleChange }
+            data-testid="password-input"
+            value={ senha }
+            name="senha"
+            type="password"
+          />
+          <button
+            type="submit"
+            label="button"
+            disabled={ !this.handleDisabled() }
+          >
+            Entrar
+          </button>
         </fieldset>
       </div>
     );
