baf4f71f91907ed1790243d63d1133f90dcfee6a
Alessandro Hugen <alessandrohugen@gmail.br>
2021-08-30 17:19:54 -0300

    Requisito 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+export const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+export default function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case INITIAL_STATE:
+    return state;
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+export const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+export default function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case INITIAL_STATE:
+    return state;
+  default:
+    return state;
+  }
+}
