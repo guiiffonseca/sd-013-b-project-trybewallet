23631b1a9ee2dbbea28f7ac3d8f082bd3690836d
Jonmorais <jonathan_ms15@hotmail.com>
2021-08-30 18:10:05 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Input from '../components/Input'; Não passa no teste :(
-import Button from '../components/Button';
+// import Button from '../components/Button'; Não funciona com o disabled
 
 class Login extends React.Component {
   constructor(props) {
@@ -10,24 +11,46 @@ class Login extends React.Component {
     this.state = {
       email: '',
       password: '',
+      buttonOff: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleButton = this.handleButton.bind(this);
   }
 
   handleChange({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value });
+    this.handleButton();
+  }
+
+  handleClick() {
+    const { history } = this.props;
+    history.push('/carteira');
+  }
+
+  handleButton() {
+    const { email, password, buttonOff } = this.state;
+    const regex = /\S+@\S+\.\S+/; // Font: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    const testEmail = regex.test(email);
+    const minLength = 5;
+    const testPassword = (password.length >= minLength);
+    if (testEmail && testPassword && buttonOff) {
+      this.setState({
+        buttonOff: false,
+      });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, buttonOff } = this.state;
     return (
       <div>
         <form>
           <input
             data-testid="email-input"
-            type="text"
+            type="email"
             name="email"
             value={ email }
             placeholder="Login"
@@ -43,10 +66,13 @@ class Login extends React.Component {
             onChange={ this.handleChange }
           />
 
-          <Button
+          <button
+            onClick={ this.handleClick }
             type="button"
-            label="Entrar"
-          />
+            disabled={ buttonOff }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
@@ -54,3 +80,7 @@ class Login extends React.Component {
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
