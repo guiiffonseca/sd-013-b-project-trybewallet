bc43b574fa88db5ba729eff1d53d2ed2b15349ba
bvottri <bruna.votre@gmail.com>
2021-08-30 18:00:34 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <fieldset>
+          Email
+          <input data-testid="email-input" name="login" type="text" label="email" />
+          Senha
+          <input data-testid="password-input" name="senha" type="password" />
+          <button type="submit" label="button">Entrar</button>
+        </fieldset>
+      </div>
+    );
   }
 }
 
