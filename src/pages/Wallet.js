475557c5c0d272f4173e3f68c1ca355f18c8bcf6
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-30 18:56:06 -0300

    requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -7,9 +7,45 @@ class Wallet extends React.Component {
     const { email } = this.props;
     return (
       <div>
-        <header data-testid="email-field">{email}</header>
-        <div data-testid="total-field">{0}</div>
-        <div data-testid="header-currency-field">BRL</div>
+        <header data-testid="email-field">
+          {email}
+          <div data-testid="total-field">{0}</div>
+          <div data-testid="header-currency-field">BRL</div>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input type="number" id="value" name="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input type="text" name="description" id="description" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select name="currency" id="currency" value="currency">
+              <option value="BRL">BRL</option>
+            </select>
+          </label>
+          <label htmlFor="paymentMethod">
+            Método de pagamento
+            <select name="paymentMethod" id="paymentMethod">
+              <option value="cash">Dinheiro</option>
+              <option value="credit">Cartão de crédito</option>
+              <option value="debit">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="category">
+            Tag:
+            <select name="category" id="category">
+              <option value="food">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="transport">Transporte</option>
+              <option value="health">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
