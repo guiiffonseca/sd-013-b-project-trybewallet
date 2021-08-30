df452b8afe750a551aa63b44564bfd65334c8388
alanmdf <alanmoreirascr@gmail.com>
2021-08-30 14:55:41 -0300

    Requisito 1 finalizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form action="">
+        <label htmlFor="email-input">
+          Email:
+          <input type="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password-input">
+          Password:
+          <input type="password" data-testid="password-input" />
+        </label>
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
