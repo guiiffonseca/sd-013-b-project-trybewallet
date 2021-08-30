baccc13bde16a652e5f88f67a84791a1f33b331b
Alex <alexcorreas1994@gmail.com>
2021-08-30 15:17:42 -0300

    Adicionando Email e PassWord e botão

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          Email
+          <input type="email" id="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="password" id="password" data-testid="password-input" />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
