2fb5da9eeab2c3d1f357b1553a247890e8f2b23c
alanmdf <alanmoreirascr@gmail.com>
2021-08-30 17:43:15 -0300

    Requisito 7 finalizado

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,19 @@
 // Coloque aqui suas actions
 export const LOGIN = 'LOGIN';
+export const GET_CURRENCIES_SUCCESS = 'GET_CURRENCIES_SUCCESS';
 
 export const login = (email) => ({
   type: LOGIN,
   email,
 });
+
+export const getCurrenciesSuccess = (currencies) => ({
+  type: GET_CURRENCIES_SUCCESS,
+  currencies: currencies.filter((currency) => currency !== 'USDT'),
+});
+
+export const getCurrenciesThunk = () => async (dispatch) => {
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const data = await response.json();
+  dispatch(getCurrenciesSuccess(Object.keys(data)));
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import AddExpense from '../components/AddExpense';
 
 class Wallet extends React.Component {
