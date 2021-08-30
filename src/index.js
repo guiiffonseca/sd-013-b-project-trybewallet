3f6ae49f230a94fbe9f72bc4dca2deda297b326a
pedrodumorro <pa.coelho.castro@gmnail.com>
2021-08-30 17:19:28 -0300

    Req 1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    {/* <Provider store={ store }> */}
+    <App />
+    {/* </Provider> */}
   </BrowserRouter>,
   document.getElementById('root'),
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,83 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pwdIsBad: true,
+      emailIsBad: true,
+      email: '',
+    };
+    this.verifyEmail = this.verifyEmail.bind(this);
+    this.verifyPwd = this.verifyPwd.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  verifyEmail({ target: { value } }) {
+    const emailRegex = /^(\S+)@((?:(?:(?!-)[a-z0-9-]{1,62}[a-z0-9])\.)+[a-z0-9]{2,12})$/;
+    if (emailRegex.test(value)) {
+      this.setState({
+        emailIsBad: false,
+        email: value,
+      });
+    } else this.setState({ emailIsBad: true });
+  }
+
+  verifyPwd({ target: { value } }) {
+    const minimumPwdLength = 6;
+
+    if (value.length >= minimumPwdLength) this.setState({ pwdIsBad: false });
+    else this.setState({ pwdIsBad: true });
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    console.log(email);
+    // const { saveEmail } = this.props;
+
+    // saveEmail(email);
+  }
+
   render() {
+    const { pwdIsBad, emailIsBad } = this.state;
+
     return (
       <form>
         <label
           htmlFor="email"
         >
+          Email:
           <input
             type="email"
-            name="name"
+            name="email"
+            onChange={ this.verifyEmail }
           />
         </label>
         <label
           htmlFor="password"
         >
+          Password:
           <input
             type="password"
             name="password"
+            onChange={ this.verifyPwd }
           />
         </label>
+        <button
+          type="button"
+          disabled={ pwdIsBad || emailIsBad }
+          // onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
       </form>
     );
   }
 }
+
+// Login.propTypes = {
+//   saveEmail: PropTypes.func.isRequired,
+// };
 
 export default Login;
