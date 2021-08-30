9edf75d2a250556d5c03f4a5b569004a33cfe97b
Henrique Cursino <henriquecursino46@gmail.com>
2021-08-30 15:34:00 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="Insira o email"
+          id="email"
+        />
+        <input
+          type="text"
+          data-testid="password-input"
+          placeholder="Insira a senha"
+          id="password"
+        />
+        <button type="button">
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
