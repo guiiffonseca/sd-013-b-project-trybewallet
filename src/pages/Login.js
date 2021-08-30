5a1467e74d6e66a5e81464d80c456f2965d68145
eaeniko <eaenikolai@gmail.com>
2021-08-30 14:38:30 -0300

    [Nikolai] Init project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <fieldset>
+        <legend>Entre com sua conta</legend>
+        <label htmlFor="email">
+          Login
+          <input
+            id="email"
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            id="password"
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </fieldset>
+    );
   }
 }
 
