4229068b802a1ff5843d3546e4527fbdbe4978da
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-08-30 16:49:01 -0300

    requisito 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
+    const { userEmail } = this.props;
     return (
-      <div>TrybeWallet</div>
+      <div>
+        {`TrybeWallet ${userEmail}`}
+      </div>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,8 +1,6 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const INITIAL_STATE = {
-
   email: '',
-
 };
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
