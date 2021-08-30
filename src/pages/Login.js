423bcf3ff20c11dc0046e6238d12da89eb57b855
Matheus <matheus.pormanhani@gmail.com>
2021-08-30 18:05:41 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <div>
+          <input
+            data-testid="email-input"
+            type="text"
+            name="email"
+            onChange={ ({ target: { name, value } }) => this.setState({ [name]: value }) }
+            placeholder="email"
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            onChange={ ({ target: { name, value } }) => this.setState({ [name]: value }) }
+            placeholder="senha"
+          />
+        </div>
+        <input
+          type="button"
+          value="Entrar"
+        />
+      </div>
+    );
   }
 }
 
