e425ea50cb8aabf7f8ccc6cc2a809fba05e7f647
Lúcio BJ <lbjunq@hotmail.com>
2021-08-30 16:25:19 -0300

    Adicionado o segundo requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
-  handleClick() {
-    this.setState({});
+  constructor() {
+    super();
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      emailOk: false,
+      passwordOk: false,
+    };
+  }
+
+  async handleEmailChange({ target }) {
+    const { value } = target;
+    this.setState({ email: value });
+    // função includes foi sugerida por Nikolai, em monitoria
+    if (value.includes('@' && '.com')) {
+      this.setState({ emailOk: true });
+    }
+  }
+
+  async handlePasswordChange({ target }) {
+    const { value } = target;
+    const minLenght = 6;
+    this.setState({ password: value });
+    if (value.length >= minLenght) {
+      this.setState({ passwordOk: true });
+    }
   }
 
   render() {
+    const { email, password, emailOk, passwordOk } = this.state;
+    const minLenght = 6;
     return (
       <div>
         <p>Hello, TrybeWallet!</p>
         Login:
-        <input type="text" data-testid="email-input" />
+        <input
+          type="text"
+          data-testid="email-input"
+          onChange={ this.handleEmailChange }
+          value={ email }
+        />
+        { (email !== '' && !email.includes('@' && '.com'))
+        && <p>Invalid email format!</p> }
         Password:
-        <input type="text" data-testid="password-input" />
-        <button type="button" disabled>Entrar</button>
+        <input
+          type="text"
+          data-testid="password-input"
+          onChange={ this.handlePasswordChange }
+          value={ password }
+        />
+        { (password.length <= minLenght && password.length !== 0)
+        && <p>Invalid Password: Too short!</p> }
+        <button
+          type="button"
+          disabled={ !(emailOk && passwordOk) }
+        >
+          Entrar
+        </button>
       </div>);
   }
 }
