465c11fc9fbf91cc6a741a50c01249f85fc8a1d1
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-08-30 17:27:06 -0300

    linter

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -12,42 +12,40 @@ class Wallet extends React.Component {
           <p data-testid="total-field">0</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
-        <div>
-          <form>
-            <label htmlFor="valor">
-              Valor
-              <input type="number" name="valor" id="valor" />
-            </label>
-            <label htmlFor="describe">
-              Descrição
-              <input type="text" name="describe" id="describe" />
-            </label>
-            <label htmlFor="moeda">
-              Moeda
-              <select name="moeda" id="moeda">
-                moedas
-              </select>
-            </label>
-            <label htmlFor="metodo-pagamento">
-              Método de pagamento
-              <select name="metodo-pagamento" id="metodo-pagamento">
-                <option value="dinheiro">Dinheiro</option>
-                <option value="credito">Cartão de crédito</option>
-                <option value="debito">Cartão de débito</option>
-              </select>
-            </label>
-            <label htmlFor="categoria">
-              Tag
-              <select name="categoria" id="categoria">
-                <option value="Alimentação">Alimentação</option>
-                <option value="Lazer">Lazer</option>
-                <option value="Trabalho">Trabalho</option>
-                <option value="Transporte">Transporte</option>
-                <option value="Saúde">Saúde</option>
-              </select>
-            </label>
-          </form>
-        </div>
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input type="number" name="valor" id="valor" />
+          </label>
+          <label htmlFor="describe">
+            Descrição
+            <input type="text" name="describe" id="describe" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select name="moeda" id="moeda">
+              moedas
+            </select>
+          </label>
+          <label htmlFor="metodo-pagamento">
+            Método de pagamento
+            <select name="metodo-pagamento" id="metodo-pagamento">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="credito">Cartão de crédito</option>
+              <option value="debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="categoria">
+            Tag
+            <select name="categoria" id="categoria">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
