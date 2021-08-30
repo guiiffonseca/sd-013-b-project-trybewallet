26d15d42187bdc661b72042d422a3d3d6344cb91
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-08-30 15:40:52 -0300

    requisito 1 - concluido

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <form className="login-container">
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={ email }
+          onChange={ this.onChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          value={ password }
+          onChange={ this.onChange }
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
