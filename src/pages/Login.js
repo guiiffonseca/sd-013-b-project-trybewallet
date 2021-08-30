73c60560136c68e670c99756d5f6120c05375b1d
FilipeGuto <filipeguto95@gmail.com>
2021-08-30 14:42:38 -0300

    Exercise 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Button from '../components/Button';
+import Input from '../components/Input';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="Login">
+        <Input
+          type="text"
+          placeholder="email"
+          data-testid="email-input"
+        />
+        <Input
+          type="password"
+          placeholder="senha"
+          data-testid="password-input"
+        />
+        <Button
+          type="button"
+          label="Entrar"
+        />
+      </div>
+    );
   }
 }
 
