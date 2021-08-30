cfd5abfa4eebcf97e1942aeb22850b62c6f402f9
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-30 15:57:39 -0300

    requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,38 @@
 // Coloque aqui suas actions
+
+export const LOADING = 'LOADING';
+export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const FETCH_ERROR = 'FETCH_ERROR';
+export const SAVE_COINS = 'SAVE_COINS';
+
+export const loading = () => ({
+  type: LOADING,
+});
+
+export const saveEmail = (email) => ({
+  type: SAVE_EMAIL,
+  payload: { email },
+});
+
+export const saveCoins = (currency) => ({
+  typeof: SAVE_COINS,
+  payload: { currency },
+});
+
+export const fetchError = (error) => ({
+  type: FETCH_ERROR,
+  payload: { error },
+});
+
+const thunkAPI = (URL) => async (dispatch) => {
+  dispatch(loading());
+  try {
+    const response = await fetch(URL);
+    const moedas = await response.json();
+    dispatch(saveCoins(moedas));
+  } catch (error) {
+    dispatch(fetchError(error));
+  }
+};
+
+export default thunkAPI;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,64 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonDisabled: true,
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    this.enableButton();
+  }
+
+  enableButton() {
+    const { email, password } = this.state;
+    const passwordMinLength = 5;
+    const emailIsValid = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i;
+    const enable = emailIsValid.test(email) && password.length >= passwordMinLength;
+    this.setState({ buttonDisabled: !enable });
+  }
+
   render() {
+    const { email, password, buttonDisabled } = this.state;
     return (
       <div>
-        <label htmlFor="email">
-          <input
-            type="text"
-            id="email"
-            name="email"
-            data-testid="email-input"
-          />
-        </label>
-        <label htmlFor="password">
-          <input
-            type="password"
-            id="password"
-            name="password"
-            data-testid="password-input"
-          />
-        </label>
-        <button
-          type="button"
-        >
-          Entrar
-        </button>
+        <form>
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={ this.handleChange }
+              value={ email }
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              value={ password }
+              minLenght="6"
+            />
+          </label>
+          <button
+            disabled={ buttonDisabled }
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
