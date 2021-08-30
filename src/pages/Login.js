45c6526f96d2547bcdb6f0e3a7ad188c9270ac59
paulomichael <paulo.michael@gmail.com>
2021-08-30 16:15:36 -0300

    requisito 1 - cria uma página inicial de login com os seguintes campos e características

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    // return <div>Login</div>;
+    return (
+      <div>
+        <h1>
+          Login
+        </h1>
+        <form>
+          <label htmlFor="email">
+            Email
+            <input data-testid="email-input" type="text" id="email" />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input data-testid="password-input" type="text" id="password" />
+          </label>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+// import { SELECT_MOVIE } from '../actions/index';
+// import categories from '../data';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
