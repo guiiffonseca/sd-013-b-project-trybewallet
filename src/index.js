5c2e44e66e658f1ccfaccb4c35c43392aa34f6bc
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-08-30 15:13:31 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+// import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  // <Provider>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  // </Provider>,
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="email">
+          <input
+            type="text"
+            id="email"
+            name="email"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
