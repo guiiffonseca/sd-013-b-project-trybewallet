f7e7def3934209ba94c1e5f657d836bdf542b2a0
Barros99 <jeanmeneses@hotmail.com>
2021-08-30 14:32:22 -0300

    iniciando o projeto

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,11 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+import { combineReducers } from 'redux';
+import issLocation from './issLocation';
+
+const rootReducer = combineReducers({
+  issLocation,
+});
+
+export default rootReducer;
