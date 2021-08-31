a9e5375b62a96447287d38bda35a4fd4f7eaeb2c
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-08-31 19:03:48 -0300

    initializing the project with settings a ReduxDevTools, store and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <label className="label-login" htmlFor="email">
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={ email }
+            data-testid="email-input"
+          />
+          Senha
+          <input
+            id="password"
+            name="password"
+            value={ password }
+            data-testid="password-input"
+          />
+          Email
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </label>
+      </div>
+    );
   }
 }
 
