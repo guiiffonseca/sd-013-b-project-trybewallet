f47422914a40356c5ec8f61a4b3cb1ac0cf2490f
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-08-31 12:22:52 -0400

    requisito 6

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <fieldset>
+        <label htmlFor="valor">
+          Valor:
+          <input type="text" name="valor" id="valor" />
+        </label>
+        <label htmlFor="descricao">
+          Descrição:
+          <input type="text" name="descricao" id="descricao" />
+        </label>
+        <label htmlFor="moeda">
+          Moeda:
+          <select name="moeda" id="moeda" aria-label="moeda" />
+        </label>
+        <label htmlFor="metodo">
+          Método de Pagamento:
+          <select name="metodo" id="metodo">
+            <option value="dinheiro">Dinheiro</option>
+            <option value="cartão de débito">Cartão de débito</option>
+            <option value="cartão de crédito">Cartão de crédito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag:
+          <select name="tag" id="tag">
+            <option value="alimentação">Alimentação</option>
+            <option value="lazer">Lazer</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="transporte">Transporte</option>
+            <option value="saúde">Saúde</option>
+          </select>
+        </label>
+      </fieldset>
+    );
+  }
+}
+
+export default Form;
