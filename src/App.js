a696dfa08336dc0d1e27188fe7079e8b07defe49
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-31 17:14:36 -0300

    primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ class Login extends React.Component {
   render() {
     return (
       <div>
-        <input type="email" testid="email-input" />
+        <input type="email" data-testid="email-input" />
         <input type="text" data-testid="password-input" />
         <button type="button">Entrar</button>
       </div>);
