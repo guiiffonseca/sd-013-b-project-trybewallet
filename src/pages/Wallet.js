a6eb5ade30c235e2e077047d18ae8753599b4a42
Alex <alexcorreas1994@gmail.com>
2021-08-31 17:20:56 -0300

    Adicionando o total das despesas

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import NewExpense from '../components/NewExpense';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
+  totalExpenses() {
+    const { expenses } = this.props;
+    const total = expenses
+      .reduce((acc, expense) => acc
+      + parseFloat(expense.value)
+      * parseFloat(expense.exchangeRates[expense.currency].ask), 0);
+    return total;
+  }
+
   render() {
     const { email } = this.props;
     return (
       <div>
         <Header
           email={ email }
-          total={ 0 }
+          total={ this.totalExpenses() }
           currency="BRL"
         />
         <NewExpense />
@@ -22,10 +31,14 @@ class Wallet extends React.Component {
 
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps)(Wallet);
