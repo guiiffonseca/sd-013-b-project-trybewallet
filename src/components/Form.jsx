c86b08903960d3d0b102a5a7adaa1d857fd00d5a
JoãoBDf <joaobdf@hotmail.com>
2021-08-31 16:11:54 -0300

    Teste Requisito 6

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,38 +4,37 @@ class Form extends Component {
   render() {
     return (
       <form>
-          <label htmlFor="Valor">
-            Valor:
-            <input type="text" id="valor" value="0.00" />
-          </label>
-          <label htmlFor="Descrição">
-            Descrição:
-            <input type="text" id="Descrição" value="Digite" />
-          </label>
-          <label htmlFor="Moeda">
-            Moeda:
-            <select type="text" id="Moeda">
-            </select>
-          </label>
-          <label htmlFor="Método de pagamento">
-            Método de Pagamento:
-            <select type="text" id="Método de pagamento">
-              <option value="Dinheiro">Dinheiro</option>
-              <option value="Credito">Cartão de crédito</option>
-              <option value="Débito">Cartão de débito</option>
-            </select>
-          </label>
-          <label htmlFor="Tag">
-            Tag:
-            <select type="text" id="Tag">
-              <option value="Alimentação">Alimentação</option>
-              <option value="Lazer">Lazer</option>
-              <option value="Trabalho">Trabalho</option>
-              <option value="Transporte">Transporte</option>
-              <option value="Saúde">Saúde</option>
-            </select>
-          </label>
-        </form>
+        <label htmlFor="Valor">
+          Valor:
+          <input type="text" id="valor" value="0.00" />
+        </label>
+        <label htmlFor="Descrição">
+          Descrição:
+          <input type="text" id="Descrição" value="Digite" />
+        </label>
+        <label htmlFor="Moeda">
+          Moeda:
+          <select type="text" id="Moeda" />
+        </label>
+        <label htmlFor="Método de pagamento">
+          Método de Pagamento:
+          <select type="text" id="Método de pagamento">
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Credito">Cartão de crédito</option>
+            <option value="Débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="Tag">
+          Tag:
+          <select type="text" id="Tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+      </form>
     );
   }
 }
