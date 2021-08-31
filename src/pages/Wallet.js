bcd812252b3f7f97e3d81fadbda0d497d5fac7ed
AC-Oliveira <allancardoso21@gmail.com>
2021-08-31 11:34:41 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -16,6 +16,40 @@ class Wallet extends React.Component {
           Câmbio:
           <span data-testid="header-currency-field">BRL</span>
         </p>
+        <form>
+          <label htmlFor="expenseValue">
+            Valor
+            <input type="text" name="expenseValue" id="expenseValue" />
+          </label>
+          <label htmlFor="expenseDescription">
+            Descrição
+            <input type="text" name="expenseDescription" id="expenseDescription" />
+          </label>
+          <label htmlFor="selectCurrency">
+            Moeda
+            <select name="selectCurrency" id="selectCurrency">
+              <option value="PVU">PVU</option>
+            </select>
+          </label>
+          <label htmlFor="paymentMethod">
+            Método de Pagamento
+            <select name="paymentMethod" id="paymentMethod">
+              <option value="cash">Dinheiro</option>
+              <option value="creditCard">Cartão de crédito</option>
+              <option value="debitCard">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="expenseTag">
+            Tag
+            <select name="selectExpenseType" id="expenseTag">
+              <option value="alimentation">Alimentação</option>
+              <option value="leisure">Lazer</option>
+              <option value="work">Trabalho</option>
+              <option value="health">Saúde</option>
+              <option value="transport">Transporte</option>
+            </select>
+          </label>
+        </form>
       </>
     );
   }
