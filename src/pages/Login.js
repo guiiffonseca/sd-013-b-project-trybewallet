17c8d4e432c4e55cb04bb92b6ae35e9c2657097c
Letícia Castro <aicitelks@gmail.com>
2021-08-31 16:00:33 -0300

    Requisito 1, teste 1/3 OK

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <h2>Login</h2>
+        <label htmlFor="email-input">
+          E-mail:
+          <input type="email" data-testid="email-input" />
+        </label>
+
+        <label htmlFor="password-input">
+          Senha:
+          <input type="password" data-testid="password-input" />
+        </label>
+      </div>
+    );
   }
 }
 
