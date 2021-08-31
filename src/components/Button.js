494ee3fb6e10fde298ee5b27e2f37f05692156eb
Alex <alexcorreas1994@gmail.com>
2021-08-31 19:33:24 -0300

    Adicionando o testeid para o botão de excluir

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 class Button extends Component {
   render() {
-    const { button, onClick, disabled } = this.props;
+    const { button, onClick, disabled, testid } = this.props;
     return (
       <div>
         <button
           type="button"
           onClick={ onClick }
           disabled={ disabled }
+          data-testid={ testid }
         >
           { button }
         </button>
@@ -20,7 +21,13 @@ class Button extends Component {
 Button.propTypes = {
   button: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  disabled: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool,
+  testid: PropTypes.string,
+};
+
+Button.defaultProps = {
+  disabled: false,
+  testid: '',
 };
 
 export default Button;
