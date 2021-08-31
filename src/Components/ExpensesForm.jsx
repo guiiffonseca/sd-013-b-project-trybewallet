cbe43bbabe9f02fc29e486a42a784f99a8a05685
Alessandro Hugen <alessandrohugen@gmail.br>
2021-08-31 19:00:58 -0300

    Lint novamente

--- src/Components/ExpensesForm.jsx
+++ src/Components/ExpensesForm.jsx
@@ -5,34 +5,34 @@ class ExpensesForm extends React.Component {
     return (
       <form>
         <label htmlFor="Valor">
-        Despesas:
-            <input type="text" name="Valor" id="Valor" />
+          Despesas:
+          <input type="text" name="Valor" id="Valor" />
         </label>
         <label htmlFor="Descrição">
-        Descrição:
-            <input type="text" name="Descrição" id="Descrição" />
+          Descrição:
+          <input type="text" name="Descrição" id="Descrição" />
         </label>
         <label htmlFor="Moeda">
-        Moeda:
-            <select name="Moeda" id="Moeda" />
-            <option>BRL</option>
+          Moeda:
+          <select name="Moeda" id="Moeda" />
+          <option>BRL</option>
         </label>
         <label htmlFor="Método de pagamento">
-        Método de pagamento:
-            <select name="Método de pagamento" id="Método de pagamento">
-            <option>Dinheiro</option>
-            <option>Cartão de crédito</option>
-            <option>Cartão de débito</option>
-            </select>
+          Método de pagamento:
+          <select name="Método de pagamento" id="Método de pagamento">
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+          </select>
         </label>
         <label htmlFor="Tag">
-            <select name="Tag" id="Tag">
-            <option>Alimentação</option>
-            <option>Lazer</option>
-            <option>Trabalho</option>
-            <option>Transporte</option>
-            <option>Saúde</option>
-            </select>
+          <select name="Tag" id="Tag">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+          </select>
         </label>
       </form>
     );
