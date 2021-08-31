41eb25855d889f0b18f27dba529ab0b12439a222
Leonardo Martins <leonardomartins07@gmail.com>
2021-08-31 18:40:46 -0300

    requisito 2 ✔

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,51 @@
 import React from 'react';
 
+const VALID_EMAIL = 'alguem@email.com';
+const VALID_PASSWORD = '123456';
+
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { type, value } = target;
+    this.setState({ [type]: value });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
         <form>
           <label htmlFor="email-input">
             Email
             <input
-              type="text"
+              type="email"
+              value={ email }
+              onChange={ this.handleChange }
               data-testid="email-input"
             />
           </label>
           <label htmlFor="password-input">
             Senha
             <input
-              type="text"
+              type="password"
+              value={ password }
+              onChange={ this.handleChange }
               data-testid="password-input"
             />
           </label>
-          <button type="button">
+          <button
+            type="button"
+            disabled={ !(VALID_PASSWORD === password && VALID_EMAIL === email) }
+          >
             Entrar
           </button>
         </form>
