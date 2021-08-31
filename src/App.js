3c5bd213dc486286ff2378e628718f57b171cc0e
LuanAlexandre <luanrb19@gmail.com>
2021-08-31 15:17:21 -0400

    Dia 16.6 - projeto-trybe-wallet: solucao do requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello, TrybeWallet!
-      </div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </main>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 class Login extends React.Component {
+  renderFormFields() {
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          placeholder="E-mail"
+        />
+        <input
+          data-testid="password-input"
+          placeholder="Senha"
+        />
+        <button type="button">Entrar</button>
+      </form>
+    );
+  }
+
   render() {
-    return <div>Login</div>;
+    return this.renderFormFields();
   }
 }
 
