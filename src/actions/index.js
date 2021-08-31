33a46aff5ca1e4b6db6a69f39aed38d7b81d6a9c
Henrique Remedios <henriqueremedios@gmail.com>
2021-08-31 18:45:52 -0300

    1. Cria uma página inicial de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions.
+export const GET_USERNAME = 'GET_USERNAME';
+
+export const getUsername = (payload) => ({
+  type: GET_USERNAME,
+  payload,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: []
+};
+
+function user(state = INITIAL_STATE, action){
+  switch (action.type) {
+    case 'QUALQUER_COISA':
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default user;
