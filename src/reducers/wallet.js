7269f5e017c566352ca85b59768474cb11cbe2a4
Jessica Naiara <jessicanaiarag@gmail.com>
2021-08-30 21:42:18 -0300

    requisito 2 e 3

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+//  import { GET_ISS_LOCATION, GET_ISS_LOCATION_SUCCESS, GET_ISS_LOCATION_FAIL } from '../actions';
+
+// const INITIAL_WALLET_STATE = {
+//   currencies: [],
+//   expenses: [],
+// };
+
+// const wallet = (state = INITIAL_WALLET_STATE, action) => {
+
+// };
+
+// export default wallet;
