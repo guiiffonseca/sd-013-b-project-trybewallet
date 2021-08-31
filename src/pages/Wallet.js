fc71217425e497894502d50e95adc20947e99cb3
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-31 15:49:17 -0300

    5 - Wallet- header com email do usuario, gastos totais e cambio

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email, currencies, expenses } = this.props;
+    return (
+      <div>
+        <header>
+          <h1>TrybeWallet</h1>
+          <p data-testid="email-field">
+            { email }
+          </p>
+          <p data-testid="total-field">
+            {
+              expenses.length <= 0 ? 0
+                : expenses.reduce((total, currentValue) => total + currentValue)
+            }
+          </p>
+          <p data-testid="header-currency-field">
+            {
+              currencies.length <= 0 ? 'BRL' : currencies
+            }
+          </p>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+  currencies: PropTypes.shape.isRequired,
+  expenses: PropTypes.shape.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  currencies: state.wallet.currencies,
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
