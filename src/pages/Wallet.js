e17229d1adfa898a228340237d9809a61a0802b1
paulomichael <paulo.michael@gmail.com>
2021-08-31 17:38:46 -0300

    requisito 5 - cria um header para a página de carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <div>
+        <h1>Email:</h1>
+        <h2 data-testid="email-field">
+          {email}
+        </h2>
+        <h1>Total:</h1>
+        <h2 data-testid="total-field">
+          {0}
+        </h2>
+        <h1>Currency:</h1>
+        <h2 data-testid="header-currency-field">
+          BRL
+        </h2>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({ user: { email } }) => ({
+  email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
