be0a0c069db1b5cab329f6965c98acf337a7c8cf
Gui-Tenari <guilherme.tenarie@gmail.com>
2021-08-31 15:53:55 -0300

    Adiciona requisito 6 - Cria forms de preenchimento de despesas

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  forms() {
+    return (
+      <form>
+        <label htmlFor="valor">
+          Valor
+          <input
+            type="number"
+            name="valor"
+            id="valor"
+          />
+        </label>
+        <label htmlFor="descrição">
+          Descrição
+          <input
+            type="text"
+            name="descrição"
+            id="descrição"
+          />
+        </label>
+        <label htmlFor="moeda">
+          Moeda
+          <select id="moeda">
+            <option name="teste" value="teste">teste</option>
+          </select>
+        </label>
+        <label htmlFor="pagamento">
+          Método de pagamento
+          <select id="pagamento">
+            <option name="Dinheiro" value="dinheiro">Dinheiro</option>
+            <option name="Cartão de Crédito" value="crédito">Cartão de Crédito</option>
+            <option name="Cartão de Débito" value="débito">Cartão de Débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Tag
+          <select id="tag">
+            <option name="Alimentação" value="ali">Alimentação</option>
+            <option name="Lazer" value="laz">Lazer</option>
+            <option name="Trabalho" value="trab">Trabalho</option>
+            <option name="Transporte" value="trans">Transporte</option>
+            <option name="Saúde" value="saud">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+
   render() {
     const { email } = this.props;
     return (
@@ -18,6 +65,7 @@ class Wallet extends React.Component {
             BRL
           </div>
         </header>
+        {this.forms()}
       </div>
     );
   }
