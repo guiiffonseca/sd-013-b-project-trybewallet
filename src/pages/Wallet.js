c0af99d4b172e8f13911d7118df7374e97b0bce4
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-08-31 16:44:06 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,40 +3,11 @@ import Formulario from '../Components/Formulario';
 import Header from '../Components/Header';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      moedas: [],
-      loading: true,
-    };
-
-    this.fetchData = this.fetchData.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  async fetchData() {
-    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
-    const data = await response.json();
-    delete data.USDT;
-    this.setState({
-      moedas: data,
-      loading: false,
-    });
-  }
-
   render() {
-    const { loading } = this.state;
-    if (loading) {
-      return <h1>Carregando</h1>;
-    }
-    const { moedas } = this.state;
     return (
       <div>
         <Header />
-        <Formulario moedas={ moedas } />
+        <Formulario />
       </div>
     );
   }
