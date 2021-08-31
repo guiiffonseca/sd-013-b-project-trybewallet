36a1673d5aebdf1cb8fb0e9fbca538fa46d1696b
Alessandro Hugen <alessandrohugen@gmail.br>
2021-08-31 18:43:52 -0300

    Ajuste Lint

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import WalletLogo from '../Images/wallet.png';
 import '../CSS/header.css';
 
 class Header extends React.Component {
-    render() {
+  render() {
     const { email } = this.props;
-      return(
-          <header className="head">
-            <img src={ WalletLogo } alt="carteira"/>
-            <h2 data-testid="email-field">{ email }</h2>
-            <h3 data-testid="total-field">Total Expenses: 0</h3>
-            <h4 data-testid="header-currency-field">BRL</h4>
-          </header>
-        )
-    };
+    return(
+      <header className="head">
+        <img src={ WalletLogo } alt="carteira"/>
+        <h2 data-testid="email-field">{ email }</h2>
+        <h3 data-testid="total-field">Total Expenses: 0</h3>
+        <h4 data-testid="header-currency-field">BRL</h4>
+      </header>
+    );
+  }
 }
+
+Header.propTypes = {
+  email: propTypes.string,
+}.isRequired;
 
 const mapStateToProps = (state) => ({
     email: state.user.email,
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ExpensesForm from "../Components/ExpensesForm";
-import Header from "../Components/Header";
+import React from 'react';
+import ExpensesForm from '../Components/ExpensesForm';
+import Header from '../Components/Header';
 
 class Wallet extends React.Component {
-    render() {
-      return(
-        <div>
-          <Header />
-          <ExpensesForm />
-        </div>
-      )
-    };
+  render() {
+    return (
+      <div>
+        <Header />
+        <ExpensesForm />
+      </div>
+    );
+  }
 }
 
 export default Wallet;
