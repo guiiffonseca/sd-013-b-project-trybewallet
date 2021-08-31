1ee4b3411edf9924c4e0fc76bb87f7456f774fc2
Leonardo <leo_mc_inter@hotmail.com>
2021-08-31 16:38:33 -0300

    Teste

---
+++ src/components/input.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+  render() {
+    const { name, role } = this.props;
+    return (
+      <label htmlFor={ name }>
+        Tag
+        <select role={ role } name={ name } id={ name }>
+          <option value="alimentacao">Alimentação</option>
+          <option value="lazer" selected>
+            Lazer
+          </option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  role: PropTypes.string.isRequired,
+};
+
+export default Input;
