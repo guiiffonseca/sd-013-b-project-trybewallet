74f3f0c071dcc6afba57386dc7ae920d520e79f7
Alex <alexcorreas1994@gmail.com>
2021-08-31 13:25:06 -0300

    Criando o Componente Select e finalizando o requisito 6

---
+++ src/components/Select.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Select extends Component {
+  render() {
+    const {
+      options,
+      labelText,
+      name,
+      testid,
+      value,
+      onChange,
+    } = this.props;
+    return (
+      <div>
+        <label htmlFor={ name }>
+          { labelText }
+        </label>
+        <select
+          id={ name }
+          value={ value }
+          data-testid={ testid }
+          onChange={ onChange }
+        >
+          { options.map((option) => <option key={ option }>{ option }</option>) }
+        </select>
+      </div>
+    );
+  }
+}
+
+Select.propTypes = {
+  labelText: PropTypes.string.isRequired,
+  name: PropTypes.isRequired,
+  value: PropTypes.isRequired,
+  testid: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Select.defaultProps = {
+  testid: '',
+};
+
+export default Select;
