50eacbca52e140bf35cb290d4a70cf8350f93636
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-30 23:26:46 -0300

    implementa form do componente wallet, requisito 6

---
+++ src/components/form.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="field-valor">
+          Valor
+          <input
+            id="field-valor"
+            type="text"
+          />
+        </label>
+        <label htmlFor="field-desc">
+          Descrição
+          <input
+            id="field-desc"
+            type="text"
+          />
+        </label>
+        <label htmlFor="select-moeda">
+          Moeda
+          <select id="select-moeda">
+            {/* <option value="BRL">BRL</option> */}
+          </select>
+        </label>
+        <label htmlFor="select-payment">
+          Método de pagamento
+          <select id="select-payment">
+            <option value="money">Dinheiro</option>
+            <option value="cardCred">Cartão de crédito</option>
+            <option value="cardDeb">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="select-tag">
+          Tag
+          <select id="select-tag">
+            <option value="food">Alimentação</option>
+            <option value="leisure">Lazer</option>
+            <option value="work">Trabalho</option>
+            <option value="transport">Transporte</option>
+            <option value="health">Saúde</option>
+          </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+export default Form;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Form from '../components/form';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      despesaTotal: 0,
+      cambio: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { despesaTotal, cambio } = this.state;
+    const { getEmail } = this.props;
+    return (
+      <>
+        <header>
+          <div>
+            <span data-testid="email-field">{ getEmail }</span>
+            <span data-testid="total-field">{ despesaTotal }</span>
+            <span data-testid="header-currency-field">{ cambio }</span>
+          </div>
+        </header>
+        <section>
+          <Form />
+        </section>
+      </>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  getEmail: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  getEmail: PropTypes.string.isRequired,
+};
