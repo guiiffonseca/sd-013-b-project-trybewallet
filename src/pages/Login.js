a8dcab8a5f47258f63afb8373111dc6ca7d31d18
Dororops <leeurw@hotmail.com>
2021-08-31 19:41:28 -0300

    Adicionando Requesito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      validation: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    },
+    () => {
+      this.checkLogin();
+    });
+  }
+
+  checkLogin() {
+    const { email, password } = this.state;
+    const minPassLength = 6;
+
+    if (email === 'alguem@email.com' && password.length >= minPassLength) {
+      this.setState({
+        validation: true,
+      });
+    } else {
+      this.setState({
+        validation: false,
+      });
+    }
+  }
+
   render() {
+    const { validation } = this.state;
     return (
       <header>
         <div>Login</div>
@@ -13,6 +50,7 @@ class Login extends React.Component {
             name="email"
             placeholder="email"
             data-testid="email-input"
+            onChange={ this.handleChange }
           />
         </label>
         <label htmlFor="password">
@@ -23,10 +61,11 @@ class Login extends React.Component {
             placeholder="password"
             data-testid="password-input"
             type="password"
+            onChange={ this.handleChange }
           />
         </label>
         <Link to="/carteira">
-          <button type="button">Entrar</button>
+          <button id="send-button" type="button" disabled={ !validation }>Entrar</button>
         </Link>
       </header>
     );
