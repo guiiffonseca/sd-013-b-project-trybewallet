8541451e9243c1eb636557467b0495e88a33d822
eaeniko <eaenikolai@gmail.com>
2021-08-31 18:23:05 -0300

    [Nikolai] Req 7

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
 // Coloque aqui suas actions
+// import currencyAPI from '../services/currencyAPI';
+
 export const SET_EMAIL = 'SET_EMAIL';
 export const SET_WALLET = 'SET_WALLET';
+// export const GET_CURRENCY_API_SUCCESS = 'GET_CURRENCY_API_SUCCESS';
+// export const GET_CURRENCY_API_ERROR = 'GET_CURRENCY_API_ERROR';
 
 export const setEmail = (payload) => (
   {
@@ -13,6 +17,26 @@ export const setWallet = (payload) => (
     type: SET_WALLET,
     payload,
   });
+
+// export const getCurrencyAPISuccess = (payload) => ({
+//   type: GET_CURRENCY_API_SUCCESS,
+//   payload,
+// });
+
+// export const getCurrencyAPIError = (payload) => ({
+//   type: GET_CURRENCY_API_ERROR,
+//   payload,
+// });
+
+// export const getCurrencyAPI = () => async (dispatch) => {
+//   // action loading
+//   try {
+//     const response = await currencyAPI();
+//     dispatch(response);
+//   } catch (error) {
+//     dispatch(response(error));
+//   }
+// };
 // export const setPassword = () => (
 //   {
 //     type: SET_PASSWORD,
--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import currencyAPI from '../services/currencyAPI';
 
 class ExpenseForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const response = await currencyAPI();
+    const results = Object.keys(response);
+    const currencieMaxLenght = 3;
+    const currencies = results.filter((result) => result.length === currencieMaxLenght);
+    this.setState({ currencies });
+  }
+
+  tagLabel() {
+    return (
+      <label htmlFor="tag">
+        Tag:
+        <select name="tag" id="tag">
+          <option value="food">Alimentação</option>
+          <option value="leisure">Lazer</option>
+          <option value="work">Trabalho</option>
+          <option value="transport">Transporte</option>
+          <option value="health">Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
+  paymentLabel() {
+    return (
+      <label htmlFor="tag">
+        Método de Pagamento:
+        <select name="payment" id="payment">
+          <option value="dinheiro">Dinheiro</option>
+          <option value="credit-card">Cartão de crédito</option>
+          <option value="debit-card">Cartão de débito</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
+    const { currencies } = this.state;
     return (
       <form>
         <label htmlFor="valor">
@@ -15,28 +64,14 @@ class ExpenseForm extends React.Component {
         <label htmlFor="moeda">
           Moeda:
           <select name="moeda" id="moeda">
-            <option value="valor">1</option>
-            <option value="valor">2</option>
+            {/* <option value="valor">1</option> */}
+            {currencies.map((currency) => (
+              <option key={ currency }>{currency}</option>
+            ))}
           </select>
         </label>
-        <label htmlFor="tag">
-          Método de Pagamento:
-          <select name="payment" id="payment">
-            <option value="dinheiro">Dinheiro</option>
-            <option value="credit-card">Cartão de crédito</option>
-            <option value="debit-card">Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="tag">
-          Tag:
-          <select name="tag" id="tag">
-            <option value="food">Alimentação</option>
-            <option value="leisure">Lazer</option>
-            <option value="work">Trabalho</option>
-            <option value="transport">Transporte</option>
-            <option value="health">Saúde</option>
-          </select>
-        </label>
+        { this.paymentLabel() }
+        { this.tagLabel() }
       </form>
     );
   }
