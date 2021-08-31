340f3b6b91757b0a175b18a4db0188e6b4d4d1c3
Letícia Castro <aicitelks@gmail.com>
2021-08-31 16:00:10 -0300

    Reducers criados com implementação mínima

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'QUALQUER_COISA':
+      return state;
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
