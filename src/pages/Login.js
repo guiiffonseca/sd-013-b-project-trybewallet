0f912778cb66f81e4882e01623fde7a46811e820
Gustavo <gu.bressan@outlook.com>
2021-08-31 15:46:27 -0300

    requisito 1, tinha esquecido como conf store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          Login
+          <input
+            type="email"
+            name="login"
+            id="login"
+            data-testid="email-input"
+          />
+          senha
+          <input
+            type="password"
+            name="login"
+            id="login"
+            data-testid="password-input"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
