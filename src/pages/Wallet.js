12c2021f0f4a34becf1ed24645255f906ecb4272
bvottri <bruna.votre@gmail.com>
2021-08-31 01:55:24 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -14,6 +14,40 @@ class Wallet extends React.Component {
           <div data-testid="total-field">0</div>
           <div data-testid="header-currency-field">BRL</div>
         </header>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input name="value" id="value" type="text" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input name="text" id="description" type="text" />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select name="currency" id="currency">
+              <option>API</option>
+            </select>
+          </label>
+          <label htmlFor="paymentMethod">
+            Método de pagamento
+            <select id="paymentMethod" name="paymentMethod">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select name="tag" id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>);
   }
 }
