50e17bee7612e107ccd41b721e5d8182bae9f80b
JoãoBDf <joaobdf@hotmail.com>
2021-08-31 16:27:35 -0300

    Requisito 6 Finalizado

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,34 +6,34 @@ class Form extends Component {
       <form>
         <label htmlFor="Valor">
           Valor:
-          <input type="text" id="valor" value="0.00" />
+          <input type="text" id="Valor" />
         </label>
         <label htmlFor="Descrição">
           Descrição:
-          <input type="text" id="Descrição" value="Digite" />
+          <input type="text" id="Descrição" />
         </label>
         <label htmlFor="Moeda">
-          Moeda
+          Moeda:
           <select id="Moeda">
-            <option value="BRL">BRL </option>
+            <option value="BRL">BRL</option>
           </select>
         </label>
-        <label htmlFor="Método de pagamento">
+        <label htmlFor="Pagamento">
           Método de Pagamento:
-          <select type="text" id="Método de pagamento">
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Credito">Cartão de crédito</option>
-            <option value="Débito">Cartão de débito</option>
+          <select type="text" id="Pagamento">
+            <option value="BRL">Dinheiro</option>
+            <option value="BRL">Cartão de crédito</option>
+            <option value="BRL">Cartão de débito</option>
           </select>
         </label>
         <label htmlFor="Tag">
           Tag:
           <select type="text" id="Tag">
-            <option value="Alimentação">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
+            <option value="BRL">Alimentação</option>
+            <option value="BRL">Lazer</option>
+            <option value="BRL">Trabalho</option>
+            <option value="BRL">Transporte</option>
+            <option value="BRL">Saúde</option>
           </select>
         </label>
       </form>
