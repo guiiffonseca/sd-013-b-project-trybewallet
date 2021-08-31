5f268cf84da1020a26ae36fc6f3905c0c449482f
Henrique Cursino <henriquecursino46@gmail.com>
2021-08-31 16:00:32 -0300

    Requisito 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,23 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
 
 const INITIAL_STATE = {
-  email: '',
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case LOGIN:
+    return ({
+      ...state,
+      email: action.email,
+    });
   default:
     return state;
   }
