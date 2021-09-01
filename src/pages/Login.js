10ac831f6dd8056b60d41bab6a3e151030311447
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-09-01 17:11:24 -0300

    function for the button - enter - will be disable and enable

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { saveLoginName } from '../actions'; // importando da action
 
 class Login extends Component {
   constructor() {
@@ -6,11 +10,49 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
+      activateLogin: true,
     };
+    this.handleChangeLogin = this.handleChangeLogin.bind(this);
+    this.letsStart = this.initStart.bind(this);
+  }
+
+  handleChangeLogin({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.activateBtnLogin();
+  }
+
+  initStart() {
+    const { importLogin } = this.props;
+    const { email } = this.state;
+    importLogin(email);
+    this.setState({
+      shouldRedirectToCarteira: true,
+    });
+  }
+
+  activateBtnLogin() {
+    const { email, password } = this.state;
+    const minimumPw = 5;
+    const checkEmail = /.+@.+\.[A-Za-z]+$/;
+    if (password.length >= minimumPw && checkEmail.test(email)) {
+      this.setState({
+        activateLogin: false,
+      });
+    } else {
+      this.setState({
+        activateLogin: true,
+      });
+    }
   }
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, activateLogin, shouldRedirectToCarteira } = this.state;
+    if (shouldRedirectToCarteira) {
+      return <Redirect to="/carteira" />;
+    }
     return (
       <div>
         <label className="label-login" htmlFor="email">
@@ -19,6 +61,7 @@ class Login extends Component {
             type="text"
             name="email"
             value={ email }
+            onChange={ this.handleChangeLogin }
             data-testid="email-input"
           />
           Senha
@@ -26,11 +69,14 @@ class Login extends Component {
             id="password"
             name="password"
             value={ password }
+            onChange={ this.handleChangeLogin }
             data-testid="password-input"
           />
           Email
           <button
             type="button"
+            disabled={ activateLogin }
+            onClick={ this.initStart }
           >
             Entrar
           </button>
@@ -40,4 +86,12 @@ class Login extends Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  importLogin: (state) => dispatch(saveLoginName(state)),
+});
+
+Login.propTypes = {
+  importLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
