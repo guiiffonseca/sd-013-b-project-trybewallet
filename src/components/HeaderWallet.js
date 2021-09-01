ced6defa22e7c61c7a4507384ee00e0aeebd9241
LuanAlexandre <luanrb19@gmail.com>
2021-09-01 14:41:32 -0400

    Dia 16.6 - projeto-trybe-wallet: solucao do requisito 5

---
+++ src/components/HeaderWallet.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class HeaderWallet extends Component {
+  renderHeader() {
+    const { email } = this.props;
+    return (
+      <div>
+        <span data-testid="email-field">{`Email: ${email}`}</span>
+        <span data-testid="total-field">Despesa total: R$ 0,00</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </div>
+    );
+  }
+
+  render() {
+    return this.renderHeader();
+  }
+}
+
+HeaderWallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import HeaderWallet from '../components/HeaderWallet';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return <HeaderWallet email={ email } />;
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
