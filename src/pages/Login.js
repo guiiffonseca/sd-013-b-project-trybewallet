e4dfc35455f80fcf50d594321a03ec05180b6525
johntvale <johntvale@gmail.com>
2021-08-31 20:53:42 -0400

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,57 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div className="form-content">
+        <div className="icon-trybe">Login</div>
+        <form>
+          <label htmlFor="email-input">
+            <input
+              data-test-id="email-input"
+              type="text"
+              name="email"
+              id="email-input"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password-input">
+            <input
+              data-test-id="password-input"
+              type="password"
+              name="password"
+              id="password-input"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+
+    );
   }
 }
 
