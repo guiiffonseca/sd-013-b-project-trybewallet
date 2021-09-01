93aca4286fb3de7c8859e4d8ae645574f637a97c
LuanAlexandre <luanrb19@gmail.com>
2021-09-01 16:08:06 -0400

    Dia 16.6 - projeto-trybe-wallet: estilizacao do header

--- src/components/HeaderWallet.js
+++ src/components/HeaderWallet.js
@@ -5,7 +5,7 @@ export default class HeaderWallet extends Component {
   renderHeader() {
     const { email } = this.props;
     return (
-      <div>
+      <div className="header-container">
         <span data-testid="email-field">{`Email: ${email}`}</span>
         <span data-testid="total-field">Despesa total: R$ 0,00</span>
         <span data-testid="header-currency-field">BRL</span>
