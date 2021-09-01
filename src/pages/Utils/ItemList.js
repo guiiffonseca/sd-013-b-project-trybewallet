e21c495ac4eeaf14fe960a75186440f4642b0f9f
AC-Oliveira <allancardoso21@gmail.com>
2021-09-01 16:14:30 -0300

    Requisito 10 Lint

--- src/pages/Utils/ItemList.js
+++ src/pages/Utils/ItemList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-// eslint-disable-next-line import/extensions
-import { deleteExpense } from '../../actions/index.js';
+import { deleteExpense } from '../../actions/index';
 
 class ItemList extends Component {
   constructor(props) {
@@ -10,15 +9,15 @@ class ItemList extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(id) {
-    const { removeListItem } = this.props;
+  handleClick() {
+    const { removeListItem, id } = this.props;
     removeListItem(id);
   }
 
   render() {
     const { item:
       { value, description, method, currency,
-        tag, exchangeRates, id } } = this.props;
+        tag, exchangeRates } } = this.props;
     return (
       <tr>
         <td>{description}</td>
@@ -29,12 +28,10 @@ class ItemList extends Component {
         <td>{(Number(exchangeRates[currency].ask)).toFixed(2)}</td>
         <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
         <td>Real</td>
-        <button type="button">Editar</button>
+        <button type="button" data-testid="edit-btn">Editar</button>
         <button
           type="button"
-          onClick={
-            () => this.handleClick(id)
-          }
+          onClick={ this.handleClick }
           data-testid="delete-btn"
         >
           Remover
@@ -45,12 +42,13 @@ class ItemList extends Component {
 }
 
 ItemList.propTypes = {
+  id: PropTypes.number.isRequired,
   item: PropTypes.string.isRequired,
   removeListItem: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  removeListItem: (id) => (dispatch(deleteExpense(id))),
+  removeListItem: () => (dispatch(deleteExpense())),
 });
 
 export default connect(null, mapDispatchToProps)(ItemList);
