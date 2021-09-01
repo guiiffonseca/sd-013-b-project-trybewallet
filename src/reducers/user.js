6e168896821dc81685ff47200fb10494ee8248f9
Pedro <pedrorsehn@hotmail.com>
2021-09-01 17:23:03 -0300

    commit lixo

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
 const INITIAL_STATE = {
   email: '',
+  logado: false,
 };
 
 function user(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'SET_EMAIL':
     return { ...state, email: action.payload };
+  case 'SET_LOGED':
+    return { ...state, logado: true };
   default:
     return state;
   }
