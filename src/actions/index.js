50f275f0460723a856caf5ec01fbff4734a2ae23
Lucas <lucascardim777@gmail.com>
2021-09-01 15:45:51 -0300

    Requirement 1, with payload now.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,5 @@ export const USER_EMAIL = 'USER_EMAIL';
 
 export const inputEmail = (email) => ({
   type: USER_EMAIL,
-  email,
+  payload: email,
 });
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,7 +9,7 @@ const userInput = (state = INITIAL_STATE, action) => {
   case USER_EMAIL:
     return {
       ...state,
-      email: action.email,
+      email: action.payload,
     };
   default:
     return state;
