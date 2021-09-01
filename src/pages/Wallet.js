dfbd783123f2534f9ba56172aa53a5ff6b6d76bc
Thaís Sampaio <thais_castrocs@outlook.com>
2021-09-01 18:29:29 -0300

    Requisito 5 Realizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../components/header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <h2>TrybeWallet</h2>
+        <Header />
+      </div>
+    );
   }
 }
 
