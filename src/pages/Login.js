fe1f7a370edec1d66f08258e1e9c8990f51efde4
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-08-31 23:13:58 -0300

    requisito-2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,65 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusButton: true,
+      email: '',
+      senha: '',
+    };
+  }
+
+  verifyEmail = (email) => {
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    return emailRegex.test(email);
+  };
+  
+  handleEmailChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      email: value,
+    });
+    this.checkStatus();
+  };
+  
+  handlePassChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      senha: value,
+    });
+    this.checkStatus();
+  };
+  
+  checkStatus = () => {
+    const { email, senha } = this.state;
+    const status = this.verifyEmail(email);
+    const size = 5;
+    if (status === true && senha.length === size) {
+      this.setState({ statusButton: false });
+    } else {
+      this.setState({ statusButton: true });
+    }
+  };
+  
+
   render() {
+    const { statusButton, email, senha } = this.state;
     return (
       <div>
-        <input type="text" data-testid="email-input" />
-        <input type="text" data-testid="password-input" />
-        <button type="button">Entrar</button>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleEmailChange }
+        />
+        <input
+          type="text"
+          data-testid="password-input"
+          value={ senha }
+          onChange={ this.handlePassChange }
+        />
+        <button type="button" disabled={ statusButton }>Entrar</button>
       </div>
     );
   }
