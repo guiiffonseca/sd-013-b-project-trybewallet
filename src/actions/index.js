1f86f0407fc6286e10eb74f828bbaeef2ecfe326
Letícia Castro <aicitelks@gmail.com>
2021-09-01 18:22:09 -0300

    actions/index Implementada a ação da API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
 // Coloque aqui suas actions
 export const USER_EMAIL = 'USER_EMAIL';
+export const GET_CURRENCY = 'GET_CURRENCY';
 
 export const userEmail = (payload) => ({
   type: USER_EMAIL,
+  payload,
+});
+
+export const getCurrency = (payload) => ({
+  type: GET_CURRENCY,
   payload,
 });
