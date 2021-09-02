b360581e058eb3b761d92d380ac43d3ec0845602
EddieRAM <edusacarneiro@gmail.com>
2021-09-02 12:52:16 -0300

    Requirement 5

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import login from '../actions';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+    const currency = 'BRL';
+    const expenses = 0;
+    return (
+      <header>
+        <p data-testid="header-currency-field">
+          {` Choosen Currency: ${currency}`}
+        </p>
+        <p data-testid="email-field">
+          {` E-mail: ${email}`}
+        </p>
+        <p data-testid="total-field">
+          {` Total Expenses: ${expenses}`}
+        </p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  saveLogin: (email) => dispatch(login(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import login from '../actions';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <Header />);
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = ({ user: { email } }) => ({
+  email,
+});
+const mapDispatchToProps = (dispatch) => ({
+  saveLogin: (email) => dispatch(login(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
