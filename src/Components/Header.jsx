f953399ca3780d8841b2169ca6905affb60fd3cc
Alessandro Hugen <alessandrohugen@gmail.br>
2021-09-02 16:06:35 -0300

    LINT

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,7 +17,7 @@ class Header extends React.Component {
     const wExpense = wallet.expenses;
     let t = 0;
     if (wExpense) {
-      wExpense.map((e) => (t += e.value * e.exchangeRates[e.currency].ask));
+      wExpense.foEach((e) => { t += e.value * e.exchangeRates[e.currency].ask });
       return t;
     }
     return t;
