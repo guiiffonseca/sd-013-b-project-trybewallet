8813d3fa72dc8d55e6332b12a363b5f5417b9632
Bruno <brunophilippitrybe@gmail.com>
2021-09-01 23:10:59 -0300

    affzzzzz

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,27 +1,26 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { ADD_COINS, ADD_EXPENSE } from '../actions/index';
+import {
+  REQUEST_CURRENCIES,
+  RESOLVED_CURRENCIES,
+  REJECT_CURRENCIES,
+} from '../actions/index';
 
 const INITIAL_STATE = {
-      currencies: [],
-      expenses: [],
-  };
-  
-  const wallet = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-    case ADD_COINS:
-      return {
-        ...state,
-        currencies: action.payload,
-      };
-    case ADD_EXPENSE:
-      return {
-        ...state,
-        expenses: [...state.expenses, action.payload],
-      };
-    default:
-      return state;
-    }
-  };
-  
-  export default wallet;
-  
+  loadingCurrencies: false,
+  loadingExpense: false,
+  currencies: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case REQUEST_CURRENCIES:
+    return { ...state, loadingCurrencies: true };
+  case RESOLVED_CURRENCIES:
+    return { ...state, loadingCurrencies: false, currencies: action.currency };
+  case REJECT_CURRENCIES:
+  default:
+    return state;
+  }
+};
+
+export default wallet;
