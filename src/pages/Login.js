64cdedc6145f34c0f5fbd6f77f737950525a23ee
Thiago <thiagodesouza.dev@gmail.com>
2021-09-02 17:49:04 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { setEmail } = this.props;
+    dispatch(setEmail(email));
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+?$/i;
+    const testEmail = emailRegex.test(email);
+    const number = 6;
+    let enableButton = true;
+    if (password.length >= number && testEmail) {
+      enableButton = false;
+    }
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            email
+            <input
+              data-testid="email-input"
+              type="text"
+              name="email"
+              value={ email }
+              id="email"
+              placeholder="email"
+              maxLength="30"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            password
+            <input
+              data-testid="password-input"
+              type="text"
+              name="password"
+              value={ password }
+              id="password"
+              placeholder="password"
+              maxLength="6"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <Link
+            to="/carteira"
+          >
+            <button type="button" disabled={ enableButton } onClick={ this.handleClick }>
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>);
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  setEmail: (email) => dispatch(action(email)),
+});
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
