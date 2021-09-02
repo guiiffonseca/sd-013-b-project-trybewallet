b2e8578bab0702578fd5ed97a21b9aab97cc0b3c
Bruno <brunophilippitrybe@gmail.com>
2021-09-02 14:42:56 -0300

    vqv

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Header from '../components/Header';
+import CurrencyForm from '../components/CurrencyForm';
+import ExpensesTable from '../components/ExpensesTable';
+
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        <CurrencyForm />
+        <ExpensesTable />
+      </div>
+    );
   }
 }
 
