dc30557e85e2b131cc2b331f24d422f5626e2dd8
Thales <thalessilva.tv@gmail.com>
2021-09-02 13:25:36 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,56 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <form>
+        <fieldset>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+              data-testid="email-input"
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
+              data-testid="password-input"
+              required
+            />
+          </label>
+          <button type="button">
+            Entrar
+          </button>
+        </fieldset>
+      </form>
+    );
   }
 }
 
