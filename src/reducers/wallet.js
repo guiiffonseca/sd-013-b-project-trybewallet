a0e99404f6feeb80dcc6122fd894c99118b0c26f
Bruno <brunophilippitrybe@gmail.com>
2021-09-02 18:12:33 -0300

    8

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,58 +1,23 @@
-import ACTIONS from '../actions';
-
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const ADD_EXPENSE = 'ADD_EXPENSE';
+
 const INITIAL_STATE = {
-  currencies: [],
   expenses: [],
-  totalExpenses: 0,
-  isFetching: false,
-  editor: false,
-  idToEdit: 0,
 };
 
-const walletReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-  case ACTIONS.SET_CURRENCIES:
-    return { ...state, currencies: action.payload, isFetching: false };
-  case ACTIONS.SET_EXPENSE:
+function walletReducer(state = INITIAL_STATE, action) {
+  const { type, payload } = action;
+  switch (type) {
+  case ADD_EXPENSE:
     return {
       ...state,
-      expenses: [...state.expenses, action.payload],
-      isFetching: false,
+      expenses: [...state.expenses, payload.despesa],
     };
-  case ACTIONS.REMOVE_EXPENSE:
-    return {
-      ...state,
-      expenses: state.expenses.filter(
-        (expense) => expense.id !== action.payload.id,
-      ),
-    };
-  case ACTIONS.EDIT_EXPENSE:
-    return {
-      ...state,
-      expenses: state.expenses.map((expense) => {
-        if (expense.id === action.payload.id) return action.payload;
-        return expense;
-      }),
-      editor: false,
-    };
-  case ACTIONS.FETCH_API:
-    return {
-      ...state,
-      isFetching: true,
-    };
-  case ACTIONS.SET_EDITOR:
-    return {
-      ...state,
-      editor: true,
-    };
-  case ACTIONS.SET_ID_TO_EDIT:
-    return {
-      ...state,
-      idToEdit: action.payload,
-    };
+
   default:
     return state;
   }
-};
+}
+
 export default walletReducer;
