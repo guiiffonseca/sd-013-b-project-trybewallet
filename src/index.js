7e257c486b3bcec47e22ff32848cd7faf5e88d07
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-04 08:17:16 -0300

    um bocado de coisa

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Switch>
+      <Route exact path="/" component={ App } />
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root'),
 );
+
+// ReactDOM.render( <BrowserRouter> <Provider store={ store }> <App /> </Provider> </BrowserRouter>, document.getElementById('root'), ); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input type="email" data-testid="email-input" />
+          <br />
+          <input type="password" data-testid="password-input" />
+          <br />
+          <button type="button" id="input-button" disabled>Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
