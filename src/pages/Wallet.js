78231e9e7aecc5550e86516bb4a8c77d6114d98f
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-09-06 16:38:08 +0100

    requisito 7 refatorado 1.4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Form from '../components/Form';
+
 import Header from '../components/Header';
+import Form from '../components/Form';
 
 class Wallet extends React.Component {
   render() {
