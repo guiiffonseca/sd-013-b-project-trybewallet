fcc6dd735e024c6787220ecf3dfd62ae6d5dd462
José Breno <brenojbs@hotmail.com>
2021-09-06 22:09:57 -0300

    Requisito 6 pronto

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -19,9 +19,45 @@ class Wallet extends React.Component {
         <div>
           Trybe Wallet
         </div>
-        <p data-testid="email-field">{ `Usuario: ${email}` }</p>
-        <p data-testid="header-currency-field">{ `Cambio: ${cambio}` }</p>
-        <p data-testid="total-field">{ `Gasto Total: ${gastoTotal}` }</p>
+        <div>
+          <p data-testid="email-field">{ `Usuario: ${email}` }</p>
+          <p data-testid="header-currency-field">{ `Cambio: ${cambio}` }</p>
+          <p data-testid="total-field">{ `Gasto Total: ${gastoTotal}` }</p>
+        </div>
+        <form>
+          <label htmlFor="valor">
+            Valor:
+            <input type="text" id="valor" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição:
+            <input type="text" id="descrição" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select type="region" id="moeda">
+              <option value="vazio">vazio</option>
+            </select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento:
+            <select id="pagamento">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select id="tag">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </>
     );
   }
