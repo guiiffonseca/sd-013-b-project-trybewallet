d472c709e7024914a1093439bb8e69d8476f8ac5
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-09-07 13:05:35 -0300

    Requisito 01 concluido. Botao do requisito 02 a ser devidamente implementado.

--- src/index.js
+++ src/index.js
@@ -8,17 +8,26 @@ import {
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Switch>
+//       <Route exact path="/" component={ App } />
+//     </Switch>
+//   </BrowserRouter>,
+//   document.getElementById('root'),
+// );
 
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={ App } />
-    </Switch>
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
-);
-
-// ReactDOM.render( <BrowserRouter> <Provider store={ store }> <App /> </Provider> </BrowserRouter>, document.getElementById('root'), ); 
+); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,64 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      button: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    // const button = target.parentNode.lastChild;
+    // const { email, password } = this.state;
+    this.setState({
+      [name]: value,
+    });
+  }
+  
+  enableButton() {
+    const { email, password } = this.state;
+    if (!(/\S+@\S+\.\S+/.test(email) && password.length >= 6)) {
+      this.setState({ button: true });
+    } else {
+      this.setState({ button: false });
+    }
+  }
+
   render() {
+    const { email, password, button } = this.state;
     return (
       <div>
         <form>
-          <input type="email" data-testid="email-input" />
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ email }
+          />
           <br />
-          <input type="password" data-testid="password-input" />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            value={ password }  
+          />
           <br />
-          <button type="button" id="input-button" disabled>Entrar</button>
+          <button
+            type="button"
+            id="input-button"
+            disabled={ !button }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
