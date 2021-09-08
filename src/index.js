379f25f3841739e5b3b34e9d4a25fab9fdbdbd13
Bruno <brunophilippitrybe@gmail.com>
2021-09-08 14:14:09 -0300

    vqv

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
   <Provider store={ store }>
     <BrowserRouter>
       <App />
