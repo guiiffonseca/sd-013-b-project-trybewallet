a4d9108c16828ed79b8c11686850feeb8d7e46d3
Dororops <leeurw@hotmail.com>
2021-09-08 04:28:27 -0300

    Adicionando Requisito 08

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -9,7 +9,11 @@ const INITIAL_STATE = {
 function walletReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case SET_EXPENSES:
-    return { ...state, expenses: [...state.expenses, action.payload] };
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload],
+    };
+
   default:
     return state;
   }
