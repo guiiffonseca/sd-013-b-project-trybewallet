2ea2a341d1499825ae4be5edb4fc7016e3ffd43b
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-09-10 10:45:12 -0300

    Adiciona requisito 9: Ajeitando o erro do lint

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-// import '../App.css';
+import '../App.css';
 
 class Table extends React.Component {
   render() {
