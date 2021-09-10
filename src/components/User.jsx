17a744e84bdc57c06ffa753ee3bd28649e638abd
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-09-10 00:03:12 -0300

    Requisito - tabela de despesas

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -7,14 +7,37 @@ class User extends Component {
     const { expenses } = this.props;
     return (
       <div>
-        <ul>
-          { expenses.map((despesa) => (
-            <li
-              key={ despesa.id }
-            >
-              { `Descrição: ${despesa.description}, Valor: ${despesa.value}` }
-            </li>))}
-        </ul>
+        <table border="1">
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              {console.log(expense.exchangeRates[expense.currency].name)}
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.currency }</td>
+              <td>
+                { Math.round(
+                  expense.exchangeRates[expense.currency].ask * 100,
+                ) / 100 }
+              </td>
+              <td>{ expense.value * expense.exchangeRates[expense.currency].ask }</td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </table>
       </div>
     );
   }
@@ -24,8 +47,8 @@ User.propTypes = {
   expenses: PropTypes.shape(PropTypes.array).isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  expenses: state.wallet.expenses,
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
 });
 
 export default connect(mapStateToProps)(User);
