a5f4f807417d9cd0bdeb16c75d86d3d70e391d8c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-09-12 01:39:01 -0300

    App.js atualizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Login />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/carteira">
+          <Wallet />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
