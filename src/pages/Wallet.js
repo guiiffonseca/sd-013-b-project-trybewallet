7bb4819d46609785253fc94a8864fec5f70a365d
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-16 11:43:04 -0300

    Wallet.js renderizando componente Form recém criado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Form from './Wallet/Form';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: {},
+    };
+    this.getCurrencies = this.getCurrencies.bind(this);
+  }
+  
+  getCurrencies() {
+    const linkAPI = 'https://economia.awesomeapi.com.br/json/all';
+    fetch(linkAPI)
+      .then((value) => value.json())
+      .then((value) => this.setState({ currencies: value }))
+  }
+
+  componentDidMount() {
+    this.getCurrencies();
+  }
+
   render() {
     const { email } = this.props;
+    const { currencies } = this.state;
     return (
       <div>
         <h1>TrybeWallet</h1>
@@ -12,44 +33,7 @@ class Wallet extends React.Component {
         <span data-testid="total-field">0</span>
         <span data-testid="header-currency-field"> BRL</span>
         <br />
-        <form id="transaction-data">
-          <label htmlFor="expenses">
-            Valor:
-            <input type="text" id="expenses" name="expenses" />
-          </label>
-          <br />
-          <label htmlFor="description">
-            Descrição:
-            <input type="text" id="description" name="description" />
-          </label>
-          <br />
-          <label htmlFor="currency">
-            Moeda:
-            <select id="currency" form="transaction-data" name="currency">
-              <option>xablau</option>
-            </select>
-          </label>
-          <br />
-          <label htmlFor="payment-method">
-            Método de Pagamento:
-            <select id="payment-method" form="transaction-data" name="payment-method">
-              <option>Dinheiro</option>
-              <option>Cartão de Crédito</option>
-              <option>Cartão de Débito</option>
-            </select>
-          </label>
-          <br />
-          <label htmlFor="tag">
-            Tag:
-            <select id="tag" form="transaction-data" name="tag">
-              <option>Alimentação</option>
-              <option>Lazer</option>
-              <option>Trabalho</option>
-              <option>Transporte</option>
-              <option>Saúde</option>
-            </select>
-          </label>
-        </form>
+      <Form currencies={ currencies }/>
       </div>
     );
   }
