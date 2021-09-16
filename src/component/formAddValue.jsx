1e7a72de007aeaf595cecbd6c9ed0c2492f663d9
Felipe Carvalho <fecarvalho066@gmail.com>
2021-09-16 11:13:19 -0300

    Requisito 8

--- src/component/formAddValue.jsx
+++ src/component/formAddValue.jsx
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchAPI, receiveNewItem } from '../actions';
 
 class FormAddValue extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 0,
+      value: '0',
+      description: '',
+      currency: 'USD',
+      method: 'dinheiro',
+      tag: 'alimentação',
+      exchangeRates: {},
+    };
+    this.addItem = this.addItem.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const { value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async addItem() {
+    const { fetch, newItem, itemsTable, currenciesState } = this.props;
+    await fetch();
+    this.setState({
+      id: itemsTable.length,
+      exchangeRates: currenciesState,
+    });
+    newItem(this.state);
+    this.setState({
+      value: 0,
+      description: '',
+      currency: 'USD',
+      method: 'dinheiro',
+      tag: 'alimentação',
+    });
+  }
+
   render() {
     const { currenciesState } = this.props;
     const currencies = Object.keys(currenciesState);
@@ -11,18 +52,19 @@ class FormAddValue extends Component {
       <form>
         <label htmlFor="value">
           Valor:
-          {' '}
-          <input type="number" id="value" />
+          <input type="number" id="value" onChange={ this.handleChange } name="value" />
         </label>
         <label htmlFor="description">
           Descrição:
-          {' '}
-          <input type="text" id="description" />
+          <textarea
+            id="description"
+            onChange={ this.handleChange }
+            name="description"
+          />
         </label>
-        <label htmlFor="coin">
+        <label htmlFor="currency">
           Moeda:
-          {' '}
-          <select id="coin">
+          <select id="currency" onChange={ this.handleChange } name="currency">
             {currenciesFilter.map((value) => (
               <option value={ value } key={ value }>
                 { value }
@@ -30,26 +72,25 @@ class FormAddValue extends Component {
               </option>))}
           </select>
         </label>
-        <label htmlFor="payment">
+        <label htmlFor="method">
           Método de pagamento:
-          {' '}
-          <select id="payment">
-            <option value="dinheiro">Dinheiro</option>
-            <option value="crédito">Cartão de crédito</option>
-            <option value="débito">Cartão de débito</option>
+          <select id="method" onChange={ this.handleChange } name="method">
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
           </select>
         </label>
         <label htmlFor="tag">
           Tag:
-          {' '}
-          <select id="tag">
-            <option value="alimentação">Alimentação</option>
-            <option value="lazer">Lazer</option>
-            <option value="trabalho">Trabalho</option>
-            <option value="transporte">Transporte</option>
-            <option value="saúde">Saúde</option>
+          <select id="tag" onChange={ this.handleChange } name="tag">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
           </select>
         </label>
+        <button type="reset" onClick={ this.addItem }>Adicionar despesa</button>
       </form>
     );
   }
@@ -57,14 +98,26 @@ class FormAddValue extends Component {
 
 FormAddValue.propTypes = {
   currenciesState: PropTypes.arrayOf,
+  itemsTable: PropTypes.arrayOf,
+  fetch: PropTypes.func,
+  newItem: PropTypes.func,
 };
 
 FormAddValue.defaultProps = {
   currenciesState: {},
+  itemsTable: {},
+  fetch: undefined,
+  newItem: undefined,
 };
 
 const mapStateToProps = (state) => ({
   currenciesState: state.wallet.currencies,
+  itemsTable: state.wallet.expenses,
 });
 
-export default connect(mapStateToProps)(FormAddValue);
+const mapDispatchToProps = (dispatch) => ({
+  fetch: () => dispatch(fetchAPI()),
+  newItem: (state) => dispatch(receiveNewItem(state)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FormAddValue);
