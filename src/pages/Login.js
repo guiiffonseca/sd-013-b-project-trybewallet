831c3639ceab3eb87f866fdfbf603b302bd14f23
RafaelBamberg <raafaelbamberg@gmail.com>
2021-09-27 01:37:51 -0300

    Pag de login sem salvar no store o login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Login extends Component {
   constructor() {
@@ -7,23 +8,25 @@ export default class Login extends Component {
     this.state = {
       email: '',
       password: '',
+
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  verifyEmail(email) {
-    const regex = /(.*)@(.*).com/;
-    return regex.test(email);
-  };
+  // verifyEmail(email) {
+  //   const regex = /(.*)@(.*).com/;
+  //   return regex.test(email);
+  // }
 
-  verifyPassword = (password) => {
-    const MIN_LENGTH_PASSWORD = 6;
-    (password.length >= MIN_LENGTH_PASSWORD)
-  };
-  
-  loginValidator = ({ email, password }) => (
-    verifyEmail(email) && verifyPassword(password)
-  );
+  // verifyPassword(password) {
+  //   const MIN_LENGTH_PASSWORD = 6;
+  //   (password.length >= MIN_LENGTH_PASSWORD);
+  // }
+
+  // loginValidator({ email, password }) {
+  //   return verifyEmail(email) && verifyPassword(password);
+  // }
 
   handleChange({ target }) {
     const { name } = target;
@@ -32,29 +35,55 @@ export default class Login extends Component {
     });
   }
 
+  handleClick(event) {
+    event.preventDefault();
+    // const history = useHistory()
+    const { history } = this.props;
+    history.push('/carteira');
+  }
+
   render() {
     const { email, password } = this.state;
+    const PASSWORD_LENGTH = 6;
+    let valid = true;
+    if (email.includes('@' && '.com') && password.length >= PASSWORD_LENGTH) {
+      valid = false;
+    } else {
+      valid = true;
+    }
 
     return (
       <div>
         <form>
           <input
             type="text"
-            name="email-input"
+            name="email"
             data-testid="email-input"
             value={ email }
             onChange={ this.handleChange }
           />
           <input
             type="password"
-            name="password-input"
+            name="password"
             data-testid="password-input"
             value={ password }
             onChange={ this.handleChange }
           />
-          <button type="button">Entrar</button>
+          <button
+            onClick={ this.handleClick }
+            disabled={ valid }
+            type="submit"
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+// const api = 'https://economia.awesomeapi.com.br/json/all';
+
+// async function getCurrency() {
+//   const data = await fetch(api);
+//   const response = await data.json();
+
+//   return response;
+// }
