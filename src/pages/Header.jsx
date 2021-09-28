950f73fe17e9716da67d097737454f40e84b7997
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-28 16:24:50 -0300

    Adicionados novos arquivos

---
+++ src/pages/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import '../styles/Wallet.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1 className="header">TrybeWallet</h1>
+  )};
+}
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
