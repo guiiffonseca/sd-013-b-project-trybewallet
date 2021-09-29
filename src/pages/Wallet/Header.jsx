aa83e273d47ad04e9a759485ad44dc72e2b10851
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-28 23:23:17 -0300

    Header.jsx retornando informações do usuário

--- src/pages/Wallet/Header.jsx
+++ src/pages/Wallet/Header.jsx
@@ -6,16 +6,24 @@ import '../../styles/Wallet.css';
 
 class Header extends React.Component {
   render() {
-    const { currencies, expenses } = this.props;
+    const { email, currencies, expenses } = this.props;
     return (
       <div>
         <h1 className="header">TrybeWallet</h1>
+        <span data-testid="email-field">{email}</span>
+        <span
+          id="total-expenses"
+          data-testid="total-field"
+        >
+        </span>
+        <span data-testid="header-currency-field">BRL</span>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
+  email: state.user.email,
   expenses: state.wallet.expenses,
   currencies: state.wallet.currencies,
 });
