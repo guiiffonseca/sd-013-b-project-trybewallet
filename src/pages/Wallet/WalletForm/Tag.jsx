d85decbb6ddf404617e8339ea26e8e0d1611979c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-28 22:27:04 -0300

    Componente Tag implementada

--- src/pages/Wallet/WalletForm/Tag.jsx
+++ src/pages/Wallet/WalletForm/Tag.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 class Tag extends React.Component {
   render() {
     return (
-      <span>Here goes your component</span>
+      <label htmlFor="tag">
+        Tag:
+        <select
+          id="tag"
+          form="transaction-data"
+          name="tag"
+        >
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
     );
   }
 }
