302c43647967a57205ed4d97dba1c4a5736001bf
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-29 04:21:25 -0300

    Props retiradas de PurchaseTable.

--- src/pages/Wallet/PurchaseTable.jsx
+++ src/pages/Wallet/PurchaseTable.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { setExpenses } from '../../actions';
 
 class PurchaseTable extends React.Component {
   render() {
-    const { expenses } = this.props;
     return (
       <div
         id="purchase-table"
@@ -28,17 +24,4 @@ class PurchaseTable extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  expenses: state.wallet.expenses,
-  currencies: state.wallet.currencies,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  saveExpenses: (payload) => dispatch(setExpenses(payload)),
-});
-
-PurchaseTable.propTypes = {
-  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PurchaseTable);
+export default PurchaseTable;
