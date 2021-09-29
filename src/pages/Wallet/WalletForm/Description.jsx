958e1df88b080a006e1fbe525937f06ab700ae26
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-28 22:28:39 -0300

    Componente Description implementada

--- src/pages/Wallet/WalletForm/Description.jsx
+++ src/pages/Wallet/WalletForm/Description.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class Description extends React.Component {
   render() {
     return (
-      <span>Here goes your component</span>
+      <label htmlFor="description">
+        Descrição:
+        <input
+          type="text"
+          id="description"
+          name="description"
+        />
+      </label>
     );
   }
 }
