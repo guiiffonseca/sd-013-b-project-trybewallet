46b9bc42f3f9a6262cf6c984fdd9c9a5c2fd9163
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-30 06:46:12 -0300

    Correções de lint em Description.jsx

--- src/pages/Wallet/WalletForm/Description.jsx
+++ src/pages/Wallet/WalletForm/Description.jsx
@@ -19,4 +19,9 @@ class Description extends React.Component {
   }
 }
 
+Description.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
 export default Description;
