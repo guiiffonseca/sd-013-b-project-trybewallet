c4ca3a8f50d87ef1de97516f9988a85ad2083e91
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-09-30 06:49:24 -0300

    Correções de lint em PurchaseData.jsx

--- src/pages/Wallet/PurchaseTable/PurchaseData.jsx
+++ src/pages/Wallet/PurchaseTable/PurchaseData.jsx
@@ -5,27 +5,26 @@ import { setExpenses } from '../../../actions';
 
 class PurchaseTable extends React.Component {
   render() {
-    const { expenses } = this.props;
-    return(
-  //  { expenses.map((expense) => (
-  //      <tr key={ expense.id }>
-  //        <td key={ `${expense.id}-descrição` }>{ expense.description }</td>
-  //        <td key={ `${expense.id}-tag` }>{ expense.tag }</td>
-  //        <td key={ `${expense.id}-método` }>{ expense.metdod }</td>
-  //        <td key={ `${expense.id}-valor` }>{ expense.value }</td>
-  //        <td key={ `${expense.id}-moeda` }>{ expense.currency }</td>
-  //        <td key={ `${expense.id}-nome da moeda` }>{
-  //          expense.exchangeRates[expense.currency].name
-  //        }</td>
-  //        <td key={ `${expense.id}-câmbio` }>{
-  //          expense.exchangeRates[expense.currency].ask
-  //        }</td>
-  //        <td key={ `${expense.id}-moeda de conversão` }>Real</td>
-  //        <td key={ `${expense.id}-cancelar` }>Cancelar compra</td>
-  //      </tr>
-  //    ))
-  //  }
-      <p>0</p>
+    return (
+    //  { expenses.map((expense) => (
+    //      <tr key={ expense.id }>
+    //        <td key={ `${expense.id}-descrição` }>{ expense.description }</td>
+    //        <td key={ `${expense.id}-tag` }>{ expense.tag }</td>
+    //        <td key={ `${expense.id}-método` }>{ expense.metdod }</td>
+    //        <td key={ `${expense.id}-valor` }>{ expense.value }</td>
+    //        <td key={ `${expense.id}-moeda` }>{ expense.currency }</td>
+    //        <td key={ `${expense.id}-nome da moeda` }>{
+    //          expense.exchangeRates[expense.currency].name
+    //        }</td>
+    //        <td key={ `${expense.id}-câmbio` }>{
+    //          expense.exchangeRates[expense.currency].ask
+    //        }</td>
+    //        <td key={ `${expense.id}-moeda de conversão` }>Real</td>
+    //        <td key={ `${expense.id}-cancelar` }>Cancelar compra</td>
+    //      </tr>
+    //    ))
+    //  }
+      <p>{0}</p>
     );
   }
 }
