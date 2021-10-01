148b70316c867f3d58d6fb66cca98892f31ea9cd
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-01 01:54:21 -0300

    Correções de lint.

--- src/pages/Wallet/PurchaseTable/PurchaseData.jsx
+++ src/pages/Wallet/PurchaseTable/PurchaseData.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class PurchaseData extends React.Component {
   render() {
@@ -8,21 +9,21 @@ class PurchaseData extends React.Component {
       <tbody>
         { expenses.map((expense) => {
           const askPrice = Number(expense.exchangeRates[expense.currency].ask);
-            return (
-              <tr key={ expense.id }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ expense.value }</td>
-                <td>{ expense.exchangeRates[expense.currency].name }</td>
-                <td>{ Math.ceil(100 * askPrice) / 100 }</td>
-                <td>{ Math.ceil(100 * askPrice
-                  * expense.value) / 100 }</td>
-                <td>Real</td>
-                <td>Cancelar compra</td>
-              </tr>
-            );
-          })
+          return (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ expense.value }</td>
+              <td>{ expense.exchangeRates[expense.currency].name }</td>
+              <td>{ Math.ceil(100 * askPrice) / 100 }</td>
+              <td>{
+                Math.ceil(100 * askPrice * expense.value) / 100 
+              }</td>
+              <td>Real</td>
+              <td>Cancelar compra</td>
+            </tr>
+          );})
         }
       </tbody>
     );
@@ -32,5 +33,9 @@ class PurchaseData extends React.Component {
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
 });
+
+PurchaseData.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default connect(mapStateToProps, null)(PurchaseData);
