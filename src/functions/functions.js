0d052222498780d03b2f06970bf3d1b89d139698
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-01 18:48:51 -0300

    Arquivo de funções criado

---
+++ src/functions/functions.js
@@ -0,0 +1,13 @@
+const price = (expense) => (
+  expense.value * expense[expense.currency].ask
+);
+
+const summation = (array) => (
+  array.reduce((prev, next) => (prev + next), 0)
+);
+
+const roundCurrency = (value) => (
+  Math.ceil(100 * value) / 100
+);
+
+export { price, summation, roundCurrency };
