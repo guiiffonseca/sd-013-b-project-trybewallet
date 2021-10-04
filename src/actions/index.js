bc95e85ba91f9996da44ce03df7939754a891d32
Arthur Palomo <arthurpalomo@DSK-PCSSD0001.localdomain>
2021-10-04 10:23:50 -0300

    Requisito 9 Incompleto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,14 @@
 import { userAction, USER } from './userAction';
 import { walletAction, WALLET } from './walletAction';
 import { userExpenses, EXPENSES } from './userExpenses';
-// import { getWallet, GETWALLET } from './getWallet';
 
 const allActions = {
   user: userAction,
   wallet: walletAction,
   expenses: userExpenses,
-  // getWallet,
   USER,
   WALLET,
   EXPENSES,
-  // GETWALLET,
 };
 
 export default allActions;
--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,22 +1,66 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Expenses extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de Pagamento</th>
-          <th>Valor</th>
-          <th>Câmbio</th>
-          <th>Valor Convertido</th>
-          <th>Moeda de Conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de Pagamento</th>
+            <th>Valor</th>
+            <th>Câmbio</th>
+            <th>Valor Convertido</th>
+            <th>Moeda de Conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* feito com ajuda do John Torres */}
+          {console.log(expenses)}
+          {
+            expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{expense.value}</td>
+                <td>{expense.exchangeRates[expense.currency].name.split('/')[0]}</td>
+                <td>
+                  {
+                    parseFloat(expense.exchangeRates[expense.currency].ask).toFixed(2)
+                  }
+                </td>
+                <td>
+                  {parseFloat(expense.value * expense.exchangeRates[expense.currency].ask)
+                    .toFixed(2)}
+                </td>
+                <td>Real</td>
+                <td>Editar/Excluir</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     );
   }
 }
 
-export default Expenses;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+// function mapStateToProps(state) {
+//   return {
+//     expenses: state.wallet.expenses,
+//   };
+// }
+
+Expenses.propTypes = {
+  expenses: PropTypes.shape().isRequired,
+};
+export default connect(mapStateToProps, null)(Expenses);
