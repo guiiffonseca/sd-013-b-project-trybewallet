f9917838d7b7c2a99084318bde13ade6c95845ae
Arthur Palomo <arthurpalomo@DSK-PCSSD0001.localdomain>
2021-10-04 10:27:57 -0300

    Requisito 9 correção

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -11,11 +11,12 @@ class Expenses extends React.Component {
           <tr>
             <th>Descrição</th>
             <th>Tag</th>
-            <th>Método de Pagamento</th>
+            <th>Método de pagamento</th>
             <th>Valor</th>
-            <th>Câmbio</th>
-            <th>Valor Convertido</th>
-            <th>Moeda de Conversão</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
         </thead>
